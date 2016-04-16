4953e1b27f8dd99207e14eefdf92b3f0fa75a0ae
Adam Mitchell <adammitchell93@gmail.com>
2016-04-16 01:05:06 -0600

    firxed testing script

--- my_test_script.sh
+++ my_test_script.sh
@@ -16,43 +16,43 @@ declare -a runs=(
 	'100 4 .01 .01 .01 12 0'
 	);
 
-	printf "\n${RED}Reoder Tests For Part 1, no output means it's working.${NC}\n"
-	# for i in "${runs[@]}"
-	# do
-	# 	echo "\tTest: $i"
-	# 	./elevator_part_1 $i | ./reorder | ./double-check
-	# done
+printf "\n${RED}Reoder Tests For Part 1, no output means it's working.${NC}\n"
+for i in "${runs[@]}"
+do
+	printf "\tTest: $i"
+	./elevator_part_1 $i | ./reorder | ./double-check
+done
 
-	printf "\n${RED}Reoder Tests For Part 2, no output means it's working.${NC}\n"
-	for i in "${runs[@]}"
-	do
-		echo "\tTest: $i"
-		./elevator_part_2 $i | ./reorder | ./double-check
-	done
+printf "\n${RED}Reoder Tests For Part 2, no output means it's working.${NC}\n"
+for i in "${runs[@]}"
+do
+	printf "\tTest: $i"
+	./elevator_part_2 $i | ./reorder | ./double-check
+done
 
-# printf "${RED}My elevator #1${NC}\n"
-# for i in `seq 1 8`;
-# do
-# 	 ./run_and_time.sh ./elevator_part_1 $i $nruns 0
-# done
-#
-# printf "\n${RED}His elevator #1${NC}\n"
-# for i in `seq 1 8`;
-# do
-# 	 ./run_and_time.sh ./his_part_1 $i $nruns 0
-# done
+printf "${RED}My elevator #1${NC}\n"
+for i in `seq 1 8`;
+do
+	./run_and_time.sh ./elevator_part_1 $i $nruns 0
+done
 
-# printf "\n${RED}My elevator #2${NC}\n"
-# for i in `seq 1 8`;
-# do
-# 	./run_and_time.sh ./elevator_part_2 $i $nruns 0
-# done
-#
-# printf "\n${RED}His elevator #2${NC}\n"
-# for i in `seq 1 8`;
-# do
-# 	./run_and_time.sh ./his_part_2 $i $nruns 0
-# done
+printf "\n${RED}His elevator #1${NC}\n"
+for i in `seq 1 8`;
+do
+	./run_and_time.sh ./his_part_1 $i $nruns 0
+done
+
+printf "\n${RED}My elevator #2${NC}\n"
+for i in `seq 1 8`;
+do
+	./run_and_time.sh ./elevator_part_2 $i $nruns 0
+done
+
+printf "\n${RED}His elevator #2${NC}\n"
+for i in `seq 1 8`;
+do
+	./run_and_time.sh ./his_part_2 $i $nruns 0
+done
 
 # printf "\n${RED}My elevator #3${NC}\n"
 # for i in `seq 1 8`;
